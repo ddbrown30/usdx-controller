ddc8b50c4b0c6572e50773974ee6acf372f9524d
ddbrown30 <ddbrown30@users.noreply.github.com>
2026-08-05 16:48:55 -0700

    Made the colours try to match the style of Ultrastar

--- launch_usdx.bat
+++ launch_usdx.bat
@@ -3,8 +3,6 @@
 REM Start in the USDX directory
 cd /d "E:\Projects\USDX\game"
 
-REM Launch UltraStar Deluxe
-start "" "E:\Projects\USDX\game\ultrastardx.exe"
 
 REM Launch the Python application through the environment launcher
 call "%~dp0launch_env.bat" py app.py
